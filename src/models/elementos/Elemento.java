67b168f2751f6050a5a10d6ce09311de62bd6be8
Rafael Santos <154897538+NakyR19@users.noreply.github.com>
2024-09-18 11:46:13 -0300

    Update Elemento.java

--- src/models/elementos/Elemento.java
+++ src/models/elementos/Elemento.java
@@ -1,5 +1,14 @@
 package models.elementos;
+import java.awt.Graphics;
 
 public abstract class Elemento {
-    
+    protected int x, y; // Posição no tabuleiro
+
+    public Elemento(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // Método abstrato para desenhar o elemento
+    public abstract void desenhar(Graphics g);
 }
