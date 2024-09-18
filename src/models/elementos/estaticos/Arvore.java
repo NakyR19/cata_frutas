10aba06bbb6cbb49a24e5a85bb70ef8cf7518c93
Rafael Santos <154897538+NakyR19@users.noreply.github.com>
2024-09-18 11:54:26 -0300

    Update Arvore.java

--- src/models/elementos/estaticos/Arvore.java
+++ src/models/elementos/estaticos/Arvore.java
@@ -1,5 +1,26 @@
 package models.elementos.estaticos;
+import java.awt.Color;
+import java.awt.Graphics;
 
-public class Arvore extends ElemEstatico{
-    
+public class Arvore extends Elemento {
+
+    public Arvore(int x, int y) {
+        super(x, y);
+    }
+
+    // Desenha uma pi... arvore como um quadrado verde no tabuleiro
+    @Override
+    public void desenhar(Graphics g) {
+        int tamanhoCelula = 60; // Tamanho de cada célula no grid
+        int xPos = x * tamanhoCelula;
+        int yPos = y * tamanhoCelula;
+
+        // Fixando cor e desenhando um quadrado para a árvore
+        g.setColor(Color.GREEN);
+        g.fillRect(xPos, yPos, tamanhoCelula, tamanhoCelula);
+
+        // Desenhando borda ao redor da célula
+        g.setColor(Color.BLACK);
+        g.drawRect(xPos, yPos, tamanhoCelula, tamanhoCelula);
+    }
 }
