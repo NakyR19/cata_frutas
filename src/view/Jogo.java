8b6c657fac3f41c88e5d71b955edb6d326b817ea
Rafael Santos <154897538+NakyR19@users.noreply.github.com>
2024-09-18 11:52:44 -0300

    Create Jogo.java

---
+++ src/view/Jogo.java
@@ -0,0 +1,15 @@
+package view;
+import javax.swing.JFrame;
+
+public class Jogo extends JFrame {
+    public Jogo() {
+        // Fixando a dimensão da floresta como 6, tornar dinamico posteriormente
+        add(new Floresta(6));
+        setTitle("Floresta");
+        setSize(400, 400);
+        setVisible(true);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+    }
+}
