b0bd7e96d96b716a796539db88ef7dcfe9fad390
Rafael Santos <154897538+NakyR19@users.noreply.github.com>
2024-09-20 13:34:48 -0300

    Create PedraComponent.java

---
+++ src/view/elementos/estatico/PedraComponent.java
@@ -0,0 +1,23 @@
+package view.elementos.estatico;
+import view.elementos.ElementoComponent;
+import models.elementos.estaticos.Pedra;
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+
+public class PedraComponent extends ElementoComponent {
+    private Pedra pedra;
+    private Image imagemGrama;
+
+    public PedraComponent(Pedra p) {
+        this.pedra = p;
+        ImageIcon referencia = new ImageIcon("res/images/pedra.png");
+        imagemGrama = referencia.getImage();
+    }
+
+    @Override
+    public void desenhar(Graphics g, int x, int y, int cellSize) {
+        // Desenha a imagem da pedra na posição (x, y) com o tamanho de resolução da célula 64 x 64
+        g.drawImage(imagemGrama, x, y, cellSize, cellSize, null);
+    }
+}
