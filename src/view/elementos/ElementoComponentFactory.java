fbe0e6377d15a28c29de9311e6d43b02089c4f53
Rafael Santos <154897538+NakyR19@users.noreply.github.com>
2024-09-20 13:40:22 -0300

    Update ElementoComponentFactory.java

--- src/view/elementos/ElementoComponentFactory.java
+++ src/view/elementos/ElementoComponentFactory.java
@@ -2,13 +2,19 @@ package view.elementos;
 
 import models.elementos.Elemento;
 import models.elementos.estaticos.Grama;
+import models.elementos.estaticos.Pedra;
 import view.elementos.estatico.GramaComponent;
+import view.elementos.estatico.PedraComponent;
+
 import java.awt.Graphics;
 
 public class ElementoComponentFactory {
     public static ElementoComponent criarComponente(Elemento elemento) {
         if (elemento instanceof Grama) {
             return new GramaComponent((Grama) elemento);
+        }
+        if (elemento instanceof Pedra) {
+            return new PedraComponent((Pedra) elemento);
         }
         // ADICIONAR OS OUTROS ELEMENTOS
         return null; // Retorna nulo SE NÃO encontrar um componente para o elemento
