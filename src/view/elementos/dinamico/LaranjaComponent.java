091eec79a1c5ea7fc170faf0d2474e677ad19323
redbdb <gustavoasamaral@gmail.com>
2024-10-08 10:52:15 -0300

    Marcuja e laranja component, meio feitos

--- src/view/elementos/dinamico/LaranjaComponent.java
+++ src/view/elementos/dinamico/LaranjaComponent.java
@@ -1,5 +1,88 @@
 package view.elementos.dinamico;
 
-public class LaranjaComponent {
+import view.elementos.ElementoComponent;
+import models.elementos.dinamicos.Laranja;
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+
+/**
+ * A classe LaranjaComponent representa um componente gráfico dinâmico
+ * que exibe uma imagem de um objeto do tipo laranja na interface gráfica.
+ * Herda da classe ElementoComponent para reutilizar funcionalidades.
+ * 
+ * @author redbdb - gustavo assunção
+ */
+
+public class LaranjaComponent extends ElementoComponent{
     
+    /**
+     * laranja - Referência ao objeto do tipo laranja.
+     * imagemLaranja - Imagem que representa o objeto laranja.
+     */
+
+    private Laranja laranja;
+    private Image imagemLaranja;
+
+    /**
+     * Construtor da classe que inicializa o componente gráfico.
+     * @param c O objeto Laranja associado a este componente.
+     */
+
+    public LaranjaComponent(Laranja l){
+        this.laranja = l;
+        ImageIcon referencia = new ImageIcon("");//colocar a referência depois
+        imagemLaranja = referencia.getImage();
+    }
+
+    /**
+     * Desenha o objeto Laranja na interface gráfica.
+     * 
+     * @param g       O objeto Graphics usado para desenhar   
+     * @param x       A coordenada x onde será desenhado 
+     * @param y       A coordenada y onde será desenhada 
+     * @param cellSize O tamanho da célula onde o objeto será desenhado
+     */
+
+    @Override
+    public void desenhar(Graphics g, int x, int y, int cellSize){
+        g.drawImage(imagemLaranja, x, y, cellSize, cellSize, null);
+    }
+
+    /**
+     * Pega o objeto Laranja associado a este componente.
+     * @return O objeto Laranja.
+     */
+
+    public Laranja getLaranja(){
+        return laranja;
+    }
+
+    /** 
+     * Define o objeto Laranja associado a este componente.
+     * @return A imagem do objeto Laranja.
+     */
+
+    public void setLaranja(Laranja l){
+        this.laranja = l;
+    }
+
+    /**
+     * Pega a imagem do obejto Laranja associada a este componente.
+     * @return A imagem do objeto Laranja.
+     */
+
+    public Image getImageLaranja(){
+        return imagemLaranja;
+    }
+
+    /**
+     * Define a imagem do objeto Laranja associado a este componente.
+     * @return  imagemLaranja A nova imagem do objeto Laranja.
+     */
+
+    public void setImagemLaranja(Image imgL){
+        this.imagemLaranja = imgL;
+    }
+
 }
--- src/view/elementos/dinamico/MaracujaComponent.java
+++ src/view/elementos/dinamico/MaracujaComponent.java
@@ -1,5 +1,88 @@
 package view.elementos.dinamico;
 
-public class MaracujaComponent {
+import view.elementos.ElementoComponent;
+import models.elementos.dinamicos.Maracuja;
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+
+/**
+ * A classe MaracujaComponent representa um componente gráfico dinâmico
+ * que exibe uma imagem de um objeto do tipo Maracuja na interface gráfica.
+ * Herda da classe ElementoComponent para reutilizar funcionalidades.
+ * 
+ * @author redbdb - gustavo assunção
+ */
+
+public class MaracujaComponent extends ElementoComponent{
+
+    /**
+     * Maracuja - Referência ao objeto do tipo laranja.
+     * imagemMaracuja - Imagem que representa o objeto laranja.
+     */
+
+    private Maracuja maracuja;
+    private Image imagemMaracuja;
+
+    /**
+     * Construtor da classe que inicializa o componente gráfico.
+     * @param c O objeto Maracuja associado a este componente.
+     */
+
+    public MaracujaComponent(Maracuja m){
+        this.maracuja = m;
+        ImageIcon referencia = new ImageIcon("");//colocar a referência depois
+        imagemMaracuja = referencia.getImage();
+    }
+
+    /**
+     * Desenha o objeto Maracuja na interface gráfica.
+     * 
+     * @param g       O objeto Graphics usado para desenhar   
+     * @param x       A coordenada x onde será desenhado 
+     * @param y       A coordenada y onde será desenhada 
+     * @param cellSize O tamanho da célula onde o objeto será desenhado
+     */
+
+    @Override
+    public void desenhar(Graphics g, int x, int y, int cellSize){
+        g.drawImage(imagemMaracuja, x, y, cellSize, cellSize, null);
+    }
+
+    /**
+     * Pega o objeto Maracuja associado a este componente.
+     * @return O objeto Maracuja.
+     */
+
+    public Maracuja getMaracuja(){
+        return maracuja;
+    }
+
+    /** 
+     * Define o objeto Maracuja associado a este componente.
+     * @return A imagem do objeto Maracuja.
+     */
+
+    public void setMaracuja(Maracuja m){
+        this.maracuja = m;
+    }
+
+    /**
+     * Pega a imagem do obejto Maracuja associada a este componente.
+     * @return A imagem do objeto Maracuja.
+     */
+
+    public Image getImageMaracuja(){
+        return imagemMaracuja;
+    }
+
+    /**
+     * Define a imagem do objeto Maracuja associado a este componente.
+     * @return  imagemLaranja A nova imagem do objeto Maracuja.
+     */
+
+    public void setImagemMaracuja(Image imgM){
+        this.imagemMaracuja = imgM;
+    }
     
 }
